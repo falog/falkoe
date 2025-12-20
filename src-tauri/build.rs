fe11c0d645d81e3162b2f2c5d9ed3e688c5de48d
falog <fal@falog.net>
2025-12-20 19:21:26 +0900

    macos-14

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-    tauri_build::build()
+    let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let no_openmp = std::env::var("CARGO_FEATURE_NO_OPENMP").is_ok();
+
+    if target == "macos" || no_openmp {
+        println!("cargo:rustc-env=GGML_NO_OPENMP=1");
+        println!("cargo:rustc-env=WHISPER_NO_OPENMP=1");
+    }
 }
